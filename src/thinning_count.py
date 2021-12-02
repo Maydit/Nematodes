28c0fa3ae2e349063d8711cf176dc02c5b927d0c
PygMali0n <54514463+PygMali0n@users.noreply.github.com>
2021-12-02 16:41:44 -0500

    Update thinning_count.py

--- src/thinning_count.py
+++ src/thinning_count.py
@@ -7,10 +7,12 @@ from scipy.signal import convolve2d
 def thin_kernal_count(image):
     # given 2d binary tensor mask
     # to numpy and skeletonize
+    image = torch.squeeze(image)
     image = skeletonize(image.numpy())
     # apply 3x3 kernal sum
-    kernalized_image = convolve2d(image, np.ones((3,3), dtype=np.int8), mode='same', boundary='fill')
+    kernalized_image = convolve2d(image, np.ones((3,3), dtype=np.int8), mode='same', boundary='fill') == 2
+    head_tail = np.logical_and(image, kernalized_image)
     # entry with value 2 is head or tail
-    count = (kernalized_image == 2).sum() / 2
+    count = head_tail.sum() // 2
 
     return count
